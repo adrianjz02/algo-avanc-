5185e9e2f785e5e2f0c21fe28f7e3855c2e38333
AymericCadier <cadier.aymeric@gmail.com>
2024-01-25 10:10:11 +0100

    boucle

--- src/ArrayList.java
+++ src/ArrayList.java
@@ -44,19 +44,11 @@ public class ArrayList {
     public static void main(String[] args) {
         ArrayList alist = new ArrayList();
         System.out.println(alist.toString());
-        alist.ajout("test1");
+        alist.ajout("");
         System.out.println(alist.toString());
-        alist.ajout("test2");
-        alist.ajout("test3");
-        alist.ajout("test4");
-        alist.ajout("test5");
-        alist.ajout("test6");
-        alist.ajout("test7");
-        alist.ajout("test8");
-        alist.ajout("test9");
-        alist.ajout("test10");
-        alist.ajout("test11");
-        alist.ajout("test12");
+        for(int i=0 ; i<9 ;i++ ){
+            alist.ajout("test"+i);
+        }
         System.out.println(alist.toString());
         System.out.println(alist.get(0));
 
